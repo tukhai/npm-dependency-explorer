7bf0e48be4a73e461e35c76e9730e2b3a885b171
Admin <admin@Admins-MacBook-Pro.local>
2019-05-01 20:04:12 +0800

    implement react-router, add package-overview and not found page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import App from './App';
+import PackageOverview from './package-overview';
+import Notfound from './notfound';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/package-overview" component={PackageOverview} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/package-overview.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class PackageOverview extends React.Component {
+  render() {
+    return <h1>Package Overview</h1>
+  }
+}
+export default PackageOverview
