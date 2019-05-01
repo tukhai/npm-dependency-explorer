2fad0424ca82f73bbd1bf7a43b0b1d5aa1470a40
Admin <admin@Admins-MacBook-Pro.local>
2019-05-01 17:39:15 +0800

    first commit: initialize UI

--- src/App.js
+++ src/App.js
@@ -5,20 +5,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <h2>Dependency Explorer</h2>
+
+        <div className="search-container">
+          <input 
+            type={"text"}
+            // id={"myInput"}
+            className={"main-search"}
+            // onkeyup={"myFunction()"}
+            placeholder={"Keywords for NPM Packages"}
+            title={"Type in a name"}
+          />
+
+          <div className="auto-complete-items">
+            <div>react</div>
+            <div>react-router</div>
+            <div>react</div>
+            <div>react-router</div>
+          </div>
+        </div>
+
+        <button className={"search-button"}>
+          Search
+        </button>
+        
+
+        {/* <div className="auto-complete-items">
+          <div>react</div>
+          <div>react-router</div>
+          <div>react</div>
+          <div>react-router</div>
+        </div> */}
+      </div>
     </div>
   );
 }
